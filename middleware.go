cbe07cfa837b2a23f980eb4f52509d20a28dd77d
Test <test@test.com>
2026-03-08 15:45:00 +0100

    Added middleware

---
+++ middleware.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+
+	"github.com/qweasd17/gator/internal/database"
+)
+
+func middlewareLoggedIn(handler func(s *state, cmd command, user database.User) error) func(*state, command) error {
+	return func(s *state, c command) error {
+		user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+		if err != nil {
+			return err
+		}
+
+		return handler(s, c, user)
+	}
+}
+
